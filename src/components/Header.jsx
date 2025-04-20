de20fc5fb083a6abed2c006a983a0372cf01fba1
Dawit <tesfayedawit22090582@gmail.com>
2025-04-20 13:26:36 +0300

    header component added

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <div className="container gx-0 user-container">
+        <div className="user-header">
+          <div>#</div>
+          <div>Name</div>
+          <div>Email</div>
+          <div>Age</div>
+          <div>Profession</div>
+          <div>Actions</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
